6d64ddf068d687bb8618272ffe4575ceafabe10f
reooooo <70122112+reooooo@users.noreply.github.com>
2020-09-04 19:06:03 +0900

    fistcommit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,11 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.oredr(id： "DESC")
   end
+
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
+  end
+
 end
